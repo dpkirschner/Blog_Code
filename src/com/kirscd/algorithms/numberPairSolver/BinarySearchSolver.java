bbb12eaa4f0be07cd1e426da6fe6fc74a5175312
dan kirschner <dpkirschner@gmail.com>
2014-12-31 12:21:34 -0800

    fixed spacing. now persists set through runs

--- src/com/kirscd/algorithms/numberPairSolver/BinarySearchSolver.java
+++ src/com/kirscd/algorithms/numberPairSolver/BinarySearchSolver.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class BinarySearchSolver extends NumberPairSolver{
-	//O(n * log n) because you are sorting and then doing binary search on result
-	public boolean solve(ArrayList<Integer> numbers, int target) {
-		Collections.sort(numbers);
-		
-		for(int i = 0; i < numbers.size(); i++) {
-			int difference = target - numbers.get(i);
-			int start = 0;
-	        int end = numbers.size() - 1;
-	        while (start <= end) {
-	            int middle = start + (end - start) / 2;
-	            if      (difference < numbers.get(middle)) end = middle - 1;
-	            else if (difference > numbers.get(middle)) start = middle + 1;
-	            else return true;
-	        }
-		}
-		
-		return false;
-	}
+    //O(n * log n) because you are sorting and then doing binary search on result
+    public boolean solve(ArrayList<Integer> numbers, int target) {
+        Collections.sort(numbers);
+        
+        for(int i = 0; i < numbers.size(); i++) {
+            int difference = target - numbers.get(i);
+            int start = 0;
+            int end = numbers.size() - 1;
+            while (start <= end) {
+                int middle = start + (end - start) / 2;
+                if      (difference < numbers.get(middle)) end = middle - 1;
+                else if (difference > numbers.get(middle)) start = middle + 1;
+                else return true;
+            }
+        }
+        
+        return false;
+    }
 }
--- src/com/kirscd/algorithms/numberPairSolver/SetSolver.java
+++ src/com/kirscd/algorithms/numberPairSolver/SetSolver.java
@@ -5,13 +5,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetSolver extends NumberPairSolver{
-	//O(n) because you are touching every element (technically touching twice but lets not split hairs)
+	Set<Integer> set = null;
+	
+	//O(n) for initial run, O(1) for subsequent runs
 	public boolean solve(ArrayList<Integer> numbers, int target) {
-		Set<Integer> set = new HashSet<Integer>();
-		for(int i = 0; i < numbers.size(); i++) {
-			set.add(numbers.get(i));
+		//to keep the example simple I will not be clearing the set for different array inputs, but that is an edge case
+		//that should be covered if this was to see actual use
+		if(set == null) {
+			set = new HashSet<Integer>();
+			for(int i = 0; i < numbers.size(); i++) {
+				set.add(numbers.get(i));
+			}
 		}
-		
+
 		for(int i = 0; i < numbers.size(); i++) {
 			if(set.contains(target - numbers.get(i))) {
 				return true;
